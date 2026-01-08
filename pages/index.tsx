8b8511e22caa93f04725000935fbcc679e00e3ea
Nwimo Goodluck Chioma <157001681+Goodluck-creat@users.noreply.github.com>
2026-01-08 14:52:17 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -190,7 +190,7 @@ export default function Home() {
           >
            Encrypted • Starknet
             <br />
-            Beta Access Only
+           
           </p>
         </div>
       </div>
