5a77febba0f95e10d584a7647e4efd6555367348
Nwimo Goodluck Chioma <157001681+Goodluck-creat@users.noreply.github.com>
2026-01-09 12:07:13 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -159,7 +159,7 @@ export default function Home() {
          margin-top: 100px;
          height: 300px;
          position: relative;
-         background: url("control.jpg") center/cover no-repeat;
+         background: url("public/control.jpg") center/cover no-repeat;
        }
 
         .banner::after {
